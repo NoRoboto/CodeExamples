b4af23c8e3270f4d013a342b682a7730c0e849f5
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-04-15 17:09:30 -0500

    Add scripts

--- shounting-yard/main.js
+++ shounting-yard/main.js
@@ -1,6 +1,9 @@
 const algorithms = require('./algorithms/shuntingYard')
 
-const expression = process.argv[2]
+const option = process.argv[2]
+const expression = process.argv[3]
+
+const callFunction = Number(option) === 1 ? 'infixToPostfix' : 'infixToPrefix'
 
 if (expression)
-    console.log('Output', algorithms.infixToPostfix(expression))
+  console.log(algorithms[callFunction](expression))
