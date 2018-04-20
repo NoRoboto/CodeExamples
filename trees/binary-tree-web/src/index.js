6785a54620ddb94316103dd4e2cf3a3bdef6448b
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-04-19 21:57:56 -0500

    Added handler

--- trees/binary-tree-web/src/index.js
+++ trees/binary-tree-web/src/index.js
@@ -1,2 +1,22 @@
-import graph from './graph'
-import utils from './graph/utils'
+import cy from './graph'
+import getTree from './graph/utils'
+
+
+window.onload = function () {
+  var form = document.querySelector("form")
+  form.onsubmit = submitted.bind(form)
+}
+
+const submitted = (ev) => {
+  ev.preventDefault()
+  cy.remove(cy.elements())
+  const { value } = ev.target.elements[0]
+  const tree = getTree(value)
+  cy.add(tree.nodes)
+  cy.add(tree.edges)
+  const layout = cy.layout({
+    name: 'dagre'
+  })
+  layout.run()
+}
+
